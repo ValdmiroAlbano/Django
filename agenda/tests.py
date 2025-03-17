68b1152b8d1acb1dbf3553dbb3aff66683d466dd
Valdemiro <albanoquiquel@gmail.com>
2025-03-18 00:27:53 +0100

    adicionando teste no projecto

--- agenda/tests.py
+++ agenda/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from datetime import date
+
+from django.test import TestCase, Client
+from agenda.models import Categoria, Eventos
+
 
 # Create your tests here.
+class TestPaginaInicial(TestCase):
+    def test_lista_eventos(self):
+        client = Client()
+        response = client.get("/")
+        #self.assertTemplateUsed(response, "agenda/exibir_eventos.html")
+        self.assertContains(response, "<th>Nome</th>")
+
+class TestListagemEventos(TestCase):
+    def test_evento_com_data(self):
+        categoria = Categoria()
+        categoria.name = "Backend"
+        categoria.save()
+
+        evento = Eventos()
+        evento.name = "Aula python"
+        evento.categoria = categoria
+        evento.local = "Luanda"
+        evento.data = date.today()
+        evento.save()
+
+        response = client = Client()
+        self.assertContains(response, "Aula python")
+        self.assertEqual(list(response.context["eventos"]), [evento])
--- agenda/urls.py
+++ agenda/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from agenda.views import listar_eventos, exibir_evento
 
 urlpatterns = [
-    path("", listar_eventos, name = "listar_eventos"),
+    path("", listar_eventos, name = "listar_eventos_raiz"),
+    path("eventos", listar_eventos, name = "listar_eventos"),
     path("evento/<int:id>/", exibir_evento, name = "exibir_evento")
 ]
